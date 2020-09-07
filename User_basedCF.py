06bd7529871c3e5ec7caaa8751d3bf89d270b776
JacobCorner <43017899+jacobmunson@users.noreply.github.com>
2020-09-06 22:37:02 -0600

    Update User_basedCF.py

--- User_basedCF.py
+++ User_basedCF.py
@@ -14,24 +14,25 @@ class UBCollaborativeFilter(object):
         self.MAE = dict()
         self.UserMeanMatrix = None
 
-    ### 平均加权策略，预测userId对itemId的评分
+
     def getRating(self, Train_data_matrix, userId, simility_matrix, neighborset):
-        simSums = numpy.sum(simility_matrix[neighborset])  # simSums为0，即该项目尚未被其他用户评分，这里的处理方法：返回用户平均分
-        averageOfUser = self.UserMeanMatrix[userId]  # 获取userId 的平均值
-        jiaquanAverage = (Train_data_matrix[neighborset] - self.UserMeanMatrix[neighborset]).dot(simility_matrix[neighborset])  # 计算每个用户的加权，预测
+        simSums = numpy.sum(simility_matrix[neighborset])  #
+        averageOfUser = self.UserMeanMatrix[userId]  #
+        jiaquanAverage = (Train_data_matrix[neighborset] - self.UserMeanMatrix[neighborset]).dot(simility_matrix[neighborset]) 
+        #jiaquanAverage = numpy.mean(Train_data_matrix[neighborset])
         if simSums == 0:
             return averageOfUser
         else:
-            return averageOfUser + jiaquanAverage / simSums
+            return averageOfUser + jiaquanAverage / simSums # jiaquanAverage # 
 
     def doEvaluate(self, testDataMatrix, K):
         a, b = testDataMatrix.nonzero()
         for userIndex, itemIndex in zip(a, b):
-            neighborset = get_K_Neighbors(self.train_data_matrix[:, itemIndex], self.SimilityMatrix[userIndex], K)  # 用户最相似的K个用户
-            prerating = self.getRating(self.train_data_matrix[:, itemIndex], userIndex, self.SimilityMatrix[userIndex], neighborset)  # 基于训练集预测用户评分(用户数目<=K)
+            neighborset = get_K_Neighbors(self.train_data_matrix[:, itemIndex], self.SimilityMatrix[userIndex], K)  
+            prerating = self.getRating(self.train_data_matrix[:, itemIndex], userIndex, self.SimilityMatrix[userIndex], neighborset)  #
             self.truerating.append(testDataMatrix[userIndex][itemIndex])
             self.predictions.append(prerating)
             # print(len(self.predictions))
         self.RMSE[K] = RMSE(self.truerating, self.predictions)
         self.MAE[K] = MAE(self.truerating, self.predictions)
-        print("UBCF  K=%d,RMSE:%f,MAE:%f" % (K, self.RMSE[K], self.MAE[K]))
+        print("UBCF  K = %d, RMSE: %f, MAE: %f" % (K, self.RMSE[K], self.MAE[K]))
