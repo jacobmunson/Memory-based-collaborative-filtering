1faf33310911badb36bbe56210cf44aa35c4c70d
JacobCorner <43017899+jacobmunson@users.noreply.github.com>
2020-09-08 13:06:12 -0600

    Update RunExample.py

--- RunExample.py
+++ RunExample.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 import argparse
 import datetime
+import math
 
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -17,9 +19,11 @@ MovieLensData = {
     4: 'Datas/ml-20m/ratings.csv'
 }
 
-dataset = "ml-100k"
+use_cosine = True 
+use_pearson = False
+dataset = "ml-1M"
 
-def parseargs():
+def parseargs(): 
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--ratings",
@@ -64,44 +68,115 @@ if __name__ == '__main__':
 
     MyIBCF.test_data_matrix = test_data_matrix
     
-    print("Computing UBCF similarity matrix...")
-    MyUBCF.SimilityMatrix = cosine_similarity(train_data_matrix)
-    print("Computing IBCF similarity matrix...")
-    MyIBCF.SimilityMatrix = cosine_similarity(train_data_matrix.T)
- 
+    
+    if use_cosine:
+        print("Using cosine similarity...")
+        print("Computing UBCF similarity matrix...")
+        MyUBCF.SimilityMatrix = cosine_similarity(train_data_matrix)
+        print("Computing IBCF similarity matrix...")
+        MyIBCF.SimilityMatrix = cosine_similarity(train_data_matrix.T)
+    elif use_pearson:
+        print("Using pearson similarity...")
+        print("Computing UBCF similarity matrix...")
+        print("NEED TO FIX")
+        MyUBCF.SimilityMatrix = cosine_similarity(train_data_matrix)
+        print("Computing IBCF similarity matrix...")
+        MyIBCF.SimilityMatrix = cosine_similarity(train_data_matrix.T)
+        
+    
     print("Computing User Mean matrix...")
     MyUBCF.UserMeanMatrix = numpy.true_divide(MyUBCF.train_data_matrix.sum(1), (MyUBCF.train_data_matrix != 0).sum(1))  
     print("Computing Item Mean matrix...")
     MyIBCF.ItemMeanMatrix = numpy.true_divide(MyUBCF.train_data_matrix.sum(0), (MyUBCF.train_data_matrix != 0).sum(0))  
     
     MyIBCF.ItemMeanMatrix[np.isnan(MyIBCF.ItemMeanMatrix)] = 0
-    KList = [5,10,15,25, 50, 75, 100, 125]
+    KList = [5, 10, 15, 25, 50, 75, 100, 125] #  
+    df_total_ibcf = pd.DataFrame()
+    df_total_ubcf = pd.DataFrame()
     
     for i in range(len(KList)):
         startTimeLoop = datetime.datetime.now()
+        df_ibcf = pd.DataFrame()
+        df_ubcf = pd.DataFrame()
+        
+        
         MyUBCF.truerating = []
         MyUBCF.predictions = []
         MyIBCF.truerating = []
         MyIBCF.predictions = []
+        
+        MyIBCF.CI_upper = []
+        MyIBCF.CI_lower = []
+        MyUBCF.CI_upper = []
+        MyUBCF.CI_lower = []
+        
+        MyIBCF.num_nhbr_actual = []
+        MyIBCF.sd_terms = []
+        MyUBCF.num_nhbr_actual = []
+        MyUBCF.sd_terms = []
 
-        #medTime = datetime.datetime.now()
-        #print((medTime - startTime).seconds)
+        print("Starting UBCF...")
         t1 = Thread(target=MyUBCF.doEvaluate, args=(test_data_matrix, KList[i]))
-        t2 = Thread(target=MyIBCF.doEvaluate, args=(test_data_matrix, KList[i]))
         t1.start()
-        t2.start()
         t1.join()
+        
+        print("Starting IBCF...")
+        t2 = Thread(target=MyIBCF.doEvaluate, args=(test_data_matrix, KList[i]))
+        t2.start()
         t2.join()
+        
+        
+                    
+        #difference = []
+        #zip_object = zip(list1, list2)
+        #for list1_i, list2_i in zip_object:
+        #    difference.append(abs(list1_i-list2_i))
+        
+        df_ibcf["pred"] = MyIBCF.predictions
+        df_ibcf["actual"] = MyIBCF.truerating
+        df_ubcf["pred"] = MyUBCF.predictions
+        df_ubcf["actual"] = MyUBCF.truerating
+        
+        # successfully recovering RMSE and MAE
+        #print(sum(difference)/len(difference))        
+        #print(math.sqrt(sum(np.array(difference) ** 2)/len(difference)))
+        
+        #print("Attaching bounds...")
+        df_ibcf["upper"] = MyIBCF.CI_upper
+        df_ibcf["lower"] = MyIBCF.CI_lower
+        df_ubcf["upper"] = MyUBCF.CI_upper
+        df_ubcf["lower"] = MyUBCF.CI_lower        
+   
+        #print("IBCF neighbors")
+        df_ibcf["num_nbhr"] = MyIBCF.num_nhbr_actual
+        df_ibcf["K"] = KList[i]
+        
+        #print("UBCF neighbors")
+        df_ubcf["num_nbhr"] = MyUBCF.num_nhbr_actual
+        df_ubcf["K"] = KList[i]
+        
+        #print("sd terms IBCF")
+        df_ibcf["sd_terms"] = MyIBCF.sd_terms
+        #print("sd terms UBCF")
+        df_ubcf["sd_terms"] = MyUBCF.sd_terms
 
+        #print("Combining data...")
+        df_total_ubcf = pd.concat([df_total_ubcf, df_ubcf], ignore_index=True)
+        df_total_ibcf = pd.concat([df_total_ibcf, df_ibcf], ignore_index=True)
+        
         endTime = datetime.datetime.now()
-        print("Total Loop time:%d seconds " % (endTime - startTimeLoop).seconds, "| Total Cost time:%d seconds " % (endTime - startTime).seconds)
+        print("Total Loop time: %d seconds " % (endTime - startTimeLoop).seconds, "| Total Cost time: %d seconds " % (endTime - startTime).seconds)
         
         Savetxt("Docs/%s/UBCF %s %1.1f.txt" % (myparser.ratings, myparser.ratings, myparser.testsize),
                 "UBCF  K=%d\tRMSE:%f\tMAE:%f\t" % (KList[i], MyUBCF.RMSE[KList[i]], MyUBCF.MAE[KList[i]]))
         Savetxt('Docs/%s/IBCF %s %1.1f.txt' % (myparser.ratings, myparser.ratings, myparser.testsize),
                 "IBCF  K=%d\tRMSE:%f\tMAE:%f\t" % (KList[i], MyIBCF.RMSE[KList[i]], MyIBCF.MAE[KList[i]]))
     
-    
+    print('\a')
+    df_total_ibcf["model"] = "IBCF"
+    df_total_ubcf["model"] = "UBCF"
+    df_total = pd.concat([df_total_ibcf, df_total_ubcf], ignore_index=True)
+    print('\a')
     # Check performance by plotting train and test errors
     plt.plot(KList, list(MyUBCF.RMSE.values()), marker='o', label='RMSE')
     plt.plot(KList, list(MyUBCF.MAE.values()), marker='v', label='MAE')
@@ -123,3 +198,4 @@ if __name__ == '__main__':
     plt.grid()
     plt.savefig('Docs/%s/IBCF %s %1.1f.png' % (myparser.ratings, myparser.ratings, myparser.testsize))
     plt.show()
+    # df_total.to_csv("results_ml100k.csv")
