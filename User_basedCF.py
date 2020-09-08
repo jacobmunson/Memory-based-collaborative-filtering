7cbf7000f12297222aac0b224fda3d43e202d9b1
JacobCorner <43017899+jacobmunson@users.noreply.github.com>
2020-09-07 20:51:15 -0600

    add CI components

--- User_basedCF.py
+++ User_basedCF.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from DataHelper import *
 from EvaluationHelper import *
+import math
 
 
 class UBCollaborativeFilter(object):
@@ -13,6 +14,13 @@ class UBCollaborativeFilter(object):
         self.RMSE = dict()
         self.MAE = dict()
         self.UserMeanMatrix = None
+        
+        
+        self.CI_upper = []
+        self.CI_lower = []
+        
+        self.num_nhbr_actual = []
+        self.sd_terms = []
 
 
     def getRating(self, Train_data_matrix, userId, simility_matrix, neighborset):
@@ -21,8 +29,34 @@ class UBCollaborativeFilter(object):
         jiaquanAverage = (Train_data_matrix[neighborset] - self.UserMeanMatrix[neighborset]).dot(simility_matrix[neighborset]) 
         #jiaquanAverage = numpy.mean(Train_data_matrix[neighborset])
         
+        
+        self.num_nhbr_actual.append(len(neighborset)) # Train_data_matrix[neighborset]
         #print(np.std(Train_data_matrix[neighborset]))
         # Train_data_matrix[neighborset] # neighbor values - use these for CI, JK CI, BS CI
+        if len(neighborset) > 1:
+            self.sd_terms.append(np.std(Train_data_matrix[neighborset]))
+        else:
+            self.sd_terms.append(math.nan)
+        
+
+        # Train_data_matrix[neighborset] # neighbor values - use these for CI, JK CI, BS CI
+        
+        if simSums == 0 and len(neighborset) > 1:
+            self.CI_lower.append(averageOfUser - np.std(Train_data_matrix[neighborset]))
+            self.CI_upper.append(averageOfUser + np.std(Train_data_matrix[neighborset]))
+             # if sum of similarities is 0, then return average of the users
+        elif simSums != 0 and len(neighborset) > 1:
+            pred = averageOfUser + jiaquanAverage / simSums
+            self.CI_lower.append(pred - np.std(Train_data_matrix[neighborset]))
+            self.CI_upper.append(pred + np.std(Train_data_matrix[neighborset]))
+        elif simSums == 0 and len(neighborset) <= 1:
+            self.CI_lower.append(math.nan)
+            self.CI_upper.append(math.nan)
+        elif simSums != 0 and len(neighborset) <= 1:
+            self.CI_lower.append(math.nan)
+            self.CI_upper.append(math.nan)
+        
+        
                
         if simSums == 0:
             return averageOfUser
@@ -39,4 +73,4 @@ class UBCollaborativeFilter(object):
             # print(len(self.predictions))
         self.RMSE[K] = RMSE(self.truerating, self.predictions)
         self.MAE[K] = MAE(self.truerating, self.predictions)
-        print("UBCF  K = %d, RMSE: %f, MAE: %f" % (K, self.RMSE[K], self.MAE[K]))
+        print("UBCF Results:  K = %d, RMSE: %f, MAE: %f" % (K, self.RMSE[K], self.MAE[K]))
