69bbe5915d72eea73884454f14a47c590b9a79a3
JacobCorner <43017899+jacobmunson@users.noreply.github.com>
2020-09-07 20:12:25 -0600

    add confidence outlines

--- Item_basedCF.py
+++ Item_basedCF.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from DataHelper import *
 from EvaluationHelper import *
+import math
 
 
 class IBCollaborativeFilter(object):
@@ -13,9 +14,18 @@ class IBCollaborativeFilter(object):
         self.train_data_matrix = None
         self.RMSE = dict()
         self.MAE = dict()
+        
+        self.CI_upper = []
+        self.CI_lower = []
+        
+        self.num_nhbr_actual = []
+        self.sd_terms = []
+
+                
+        
 
 
-    def getRating(self, Train_data_matrix, itemId, simility_matrix, knumber=20):
+    def getRating(self, Train_data_matrix, itemId, simility_matrix, knumber = 20):
         neighborset = get_K_Neighbors(Train_data_matrix, simility_matrix, knumber) # get the neighbor set
         simSums = numpy.sum(simility_matrix[neighborset])  
         # print(simSums) Sum of similarities for weighted average
@@ -24,8 +34,37 @@ class IBCollaborativeFilter(object):
         # prediction function: p_ui = [ neighbor values - mean(of those neighbors - by neighbors) ] * sim(neighbors)
 
         #jiaquanAverage = numpy.mean(Train_data_matrix[neighborset]) # mean of neighbor values
-       # print(jiaquanAverage)
-        # 
+        
+        self.num_nhbr_actual.append(len(neighborset)) # Train_data_matrix[neighborset]
+        
+        if len(neighborset) > 1:
+            self.sd_terms.append(np.std(Train_data_matrix[neighborset]))
+        else:
+            self.sd_terms.append(math.nan)
+        
+
+        # Train_data_matrix[neighborset] # neighbor values - use these for CI, JK CI, BS CI
+        
+        if simSums == 0 and len(neighborset) > 1:
+            self.CI_lower.append(averageOfUser - np.std(Train_data_matrix[neighborset]))
+            self.CI_upper.append(averageOfUser + np.std(Train_data_matrix[neighborset]))
+             # if sum of similarities is 0, then return average of the users
+        elif simSums != 0 and len(neighborset) > 1:
+            pred = averageOfUser + jiaquanAverage / simSums
+            self.CI_lower.append(pred - np.std(Train_data_matrix[neighborset]))
+            self.CI_upper.append(pred + np.std(Train_data_matrix[neighborset]))
+        elif simSums == 0 and len(neighborset) <= 1:
+            self.CI_lower.append(math.nan)
+            self.CI_upper.append(math.nan)
+        elif simSums != 0 and len(neighborset) <= 1:
+            self.CI_lower.append(math.nan)
+            self.CI_upper.append(math.nan)
+        
+
+
+        
+        #print(jiaquanAverage)
+         
         if simSums == 0:
             return averageOfUser # if sum of similarities is 0, then return average of the users
         else:
