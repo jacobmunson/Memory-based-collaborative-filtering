185d3dce1564c578e93e3001e2571356d817f749
JacobCorner <43017899+jacobmunson@users.noreply.github.com>
2020-09-08 21:22:17 -0600

    Update User_basedCF.py

--- User_basedCF.py
+++ User_basedCF.py
@@ -3,7 +3,7 @@
 from DataHelper import *
 from EvaluationHelper import *
 import math
-
+from confidence import *
 
 class UBCollaborativeFilter(object):
     def __init__(self):
@@ -41,18 +41,18 @@ class UBCollaborativeFilter(object):
 
         # Train_data_matrix[neighborset] # neighbor values - use these for CI, JK CI, BS CI
         
-        if simSums == 0 and len(neighborset) > 1:
-            self.CI_lower.append(averageOfUser - np.std(Train_data_matrix[neighborset]))
-            self.CI_upper.append(averageOfUser + np.std(Train_data_matrix[neighborset]))
+        if simSums == 0 and len(neighborset) >= 2:
+            self.CI_lower.append(averageOfUser - standard_ci(Train_data_matrix[neighborset]))
+            self.CI_upper.append(averageOfUser + standard_ci(Train_data_matrix[neighborset]))
              # if sum of similarities is 0, then return average of the users
-        elif simSums != 0 and len(neighborset) > 1:
+        elif simSums != 0 and len(neighborset) >= 2:
             pred = averageOfUser + jiaquanAverage / simSums
-            self.CI_lower.append(pred - np.std(Train_data_matrix[neighborset]))
-            self.CI_upper.append(pred + np.std(Train_data_matrix[neighborset]))
-        elif simSums == 0 and len(neighborset) <= 1:
+            self.CI_lower.append(pred - standard_ci(Train_data_matrix[neighborset]))
+            self.CI_upper.append(pred + standard_ci(Train_data_matrix[neighborset]))
+        elif simSums == 0 and len(neighborset) < 2:
             self.CI_lower.append(math.nan)
             self.CI_upper.append(math.nan)
-        elif simSums != 0 and len(neighborset) <= 1:
+        elif simSums != 0 and len(neighborset) < 2:
             self.CI_lower.append(math.nan)
             self.CI_upper.append(math.nan)
         
