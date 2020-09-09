e75280653aadd83eb43bb18671eaa81fb75e5264
JacobCorner <43017899+jacobmunson@users.noreply.github.com>
2020-09-08 21:22:56 -0600

    Update confidence.py

--- confidence.py
+++ confidence.py
@@ -4,40 +4,25 @@ Created on Mon Sep  7 22:34:46 2020
 
 @author: Jacob
 """
-import numpy as np
-import matplotlib.pyplot as plt
-
-
 #######################################
 ### Confidence interval definitions ### 
 #######################################
+import math
+from scipy.stats import t
+import numpy as np
 
 
 ## Confidence Interval
-
-ratings  = np.array([1, 3, 1, 2, 3, 2, 5, 3, 2, 1, 3, 4])
-
+def standard_ci(ratings): # input will be Train_data_matrix[neighborset]
+    std_dev = np.std(ratings)
+    n = len(ratings)
+    if n >= 30:
+        multi = 1.96
+    else: 
+        multi = t.interval(alpha = 0.975, df = n - 1)[1] 
+    return multi * std_dev/math.sqrt(n)
 
 ## Jackknife Interval
 
 
 ## Bootstrap Interval
-
-import numpy as np
-
-ratings  = np.array([1, 3, 1, 2, 3, 2, 5, 3, 2, 1, 3, 4])
-
-ratings
-
-sample_mean = []
-for _ in range(10000):  #so B=10000
-    sample_n = np.random.choice(ratings, size = len(ratings))
-    sample_mean.append(sample_n.mean())
-    
-plt.hist(sample_mean)
-
-sample_mean
-mu_rat = ratings.mean()
-
-
-sample_mean - mu_rat
