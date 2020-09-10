3bb7de09ee942ec7d6d379bbfdb2c5e6f41af135
JacobCorner <43017899+jacobmunson@users.noreply.github.com>
2020-09-10 11:04:14 -0600

    Update confidence.py

--- confidence.py
+++ confidence.py
@@ -12,17 +12,29 @@ from scipy.stats import t
 import numpy as np
 
 
-## Confidence Interval
+#########################
+## Confidence Interval ##
+#########################
 def standard_ci(ratings): # input will be Train_data_matrix[neighborset]
     std_dev = np.std(ratings)
     n = len(ratings)
     if n >= 30:
         multi = 1.96
     else: 
-        multi = t.interval(alpha = 0.975, df = n - 1)[1] 
+        multi = t.interval(alpha = 0.975, df = n-1)[1] 
     return multi * std_dev/math.sqrt(n)
 
-## Jackknife Interval
+
+def knn_ci(ratings): # input will be Train_data_matrix[neighborset]
+    std_dev = np.std(ratings)
+    n = len(ratings)
+    multi = t.interval(alpha = 0.975, df = ((n - 2)/2))[1] 
+    return multi * std_dev/math.sqrt(n)
+
+########################
+## Jackknife Interval ##
+########################
+
 
 
 ## Bootstrap Interval
